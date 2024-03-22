08e4579e723f58e927551bbc1eb5e07ff9480358
VuDinhSon <vuson0401@gmail.com>
2024-03-22 13:35:27 +0700

    son

--- smartele/src/App.js
+++ smartele/src/App.js
@@ -1,25 +1,29 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LayoutSite from './layouts/LayoutSite';
 import RouterPublic from "./router/RouterPublic";
+import { UserProvider } from "./services/UserContext";
 
 
 function App() {
+
   return (
-  <>
-    <BrowserRouter>
-             <Routes>
-                    <Route path="/" element={<LayoutSite />}>
-                      {RouterPublic.map(function(router,index){
-                         const Page=router.component;
-                        return(    
-                          <Route key={index} path={router.path} element={<Page />} />
-                        );      
-                      })}
-                  
-                    </Route>               
-                </Routes>
-    </BrowserRouter>
-</>
+    <>
+      <UserProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LayoutSite />}>
+              {RouterPublic.map(function (router, index) {
+                const Page = router.component;
+                return (
+                  <Route key={index} path={router.path} element={<Page />} />
+                );
+              })}
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </UserProvider>
+
+    </>
   );
 }
 
